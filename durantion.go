fef8843884a547d6a01a1ce5a2970a7255334bac
fivezjd <fivezjd@gmail.com>
2024-06-24 19:00:38 +0800

    Initial commit

---
+++ durantion.go
@@ -0,0 +1,10 @@
+package duration
+
+const (
+	Second = 1
+	Minute = 60 * Second
+	Hour   = 60 * Minute
+	Day    = 24 * Hour
+	Month  = 30 * Day
+	Year   = 365 * Day
+)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/fivezjd/duration
+
+go 1.22.4
